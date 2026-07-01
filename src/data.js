0f925f571a5073e994bbca44193758a16bf6d766
Jin <simplord.189119@gmail.com>
2026-07-01 20:36:44 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const wuwaData = [
+  {
+    id: 'jinhsi',
+    name: 'Jinhsi',
+    element: 'Spectro',
+    weaponType: 'Broadblade',
+    signatureWeapon: 'Ages of Harvest',
+    rarity: 5
+  },
+  {
+    id: 'changli',
+    name: 'Changli',
+    element: 'Fusion',
+    weaponType: 'Sword',
+    signatureWeapon: 'Blazing Brilliance',
+    rarity: 5
+  },
+  {
+    id: 'camellya',
+    name: 'Camellya',
+    element: 'Havoc',
+    weaponType: 'Sword', 
+    signatureWeapon: 'Red String', 
+    rarity: 5
+  }
+];
