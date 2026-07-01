1517070cf7fa79f728ce6c2f051d39727ca20979
Jin <simplord.189119@gmail.com>
2026-07-01 23:28:54 +0530

    Create weaponsData.js

---
+++ src/weaponsData.js
@@ -0,0 +1,28 @@
+export const WEAPONS = [
+  // --- Standard 5-Star Weapons ---
+  { id: 'standard_sword', name: 'Emerald of Genesis', type: 'Sword', rarity: 5 },
+  { id: 'standard_rectifier', name: 'Cosmic Ripples', type: 'Rectifier', rarity: 5 },
+  { id: 'standard_pistol', name: 'Static Mist', type: 'Pistols', rarity: 5 },
+  { id: 'standard_broadblade', name: 'Lustrous Razor', type: 'Broadblade', rarity: 5 },
+  { id: 'standard_gauntlet', name: 'Abyss Surges', type: 'Gauntlets', rarity: 5 },
+
+  // --- Limited / Signature 5-Star Weapons ---
+  { id: 'sig_sword_verdant', name: 'Verdant Summit', type: 'Sword', rarity: 5 },
+  { id: 'sig_rectifier_string', name: 'Stringmaster', type: 'Rectifier', rarity: 5 },
+  { id: 'sig_sword_blazing', name: 'Blazing Brilliance', type: 'Sword', rarity: 5 },
+  { id: 'sig_broadblade_ages', name: 'Ages of Harvest', type: 'Broadblade', rarity: 5 },
+  { id: 'sig_gauntlet_verity', name: 'Verity\'s Handle', type: 'Gauntlets', rarity: 5 },
+  { id: 'sig_rectifier_rime', name: 'Rime-Draped Sprouts', type: 'Rectifier', rarity: 5 },
+  { id: 'sig_broadblade_tragi', name: 'Tragicomedy', type: 'Gauntlets', rarity: 5 },
+  { id: 'sig_pistol_lastdance', name: 'The Last Dance', type: 'Pistols', rarity: 5 },
+  { id: 'sig_rectifier_whispers', name: 'Whispers of Sirens', type: 'Rectifier', rarity: 5 },
+  { id: 'sig_broadblade_wildfire', name: 'Wildfire Mark', type: 'Broadblade', rarity: 5 },
+  { id: 'sig_sword_redspring', name: 'Red Spring', type: 'Sword', rarity: 5 },
+  { id: 'sig_sword_lethean', name: 'Lethean Elegy', type: 'Sword', rarity: 5 },
+  { id: 'sig_gauntlet_luminous', name: 'Luminous Hymn', type: 'Gauntlets', rarity: 5 },
+  { id: 'sig_broadblade_justice', name: 'Blazing Justice', type: 'Broadblade', rarity: 5 },
+  { id: 'sig_sword_defier', name: 'Defier\'s Thorn', type: 'Sword', rarity: 5 },
+  { id: 'sig_broadblade_unflicker', name: 'Unflickering Valor', type: 'Broadblade', rarity: 5 },
+  { id: 'sig_pistol_woodland', name: 'Woodland Aria', type: 'Pistols', rarity: 5 },
+  { id: 'sig_rectifier_lux', name: 'Lux & Umbra', type: 'Rectifier', rarity: 5 }
+];
