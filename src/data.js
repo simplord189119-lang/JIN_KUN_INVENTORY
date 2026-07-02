10d735d3611ee6f064e30995c5873ba884cee7f5
Jin <simplord.189119@gmail.com>
2026-07-02 10:55:59 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,59 +1,162 @@
-
 export const CHARACTERS = [
   // Glacio
   { id: 'baizhi', name: 'Baizhi', element: 'Glacio', weaponType: 'Rectifier', rarity: 4, image: '' },
-  { id: 'carlotta', name: 'Carlotta', element: 'Glacio', weaponType: 'Pistols', rarity: 5, image: 'https://pin.it/6el2g2Gft' },
-  { id: 'hiyuki', name: 'Hiyuki', element: 'Glacio', weaponType: 'Sword', rarity: 5, image: 'https://pin.it/67HPENI2q' },
+
+  { id: 'carlotta', name: 'Carlotta', element: 'Glacio', weaponType: 'Pistols', rarity: 5, image: '' },
+
+  { id: 'hiyuki', name: 'Hiyuki', element: 'Glacio', weaponType: 'Sword', rarity: 5, image: '' },
+
   { id: 'lingyang', name: 'Lingyang', element: 'Glacio', weaponType: 'Gauntlets', rarity: 5, image: '' },
+
   { id: 'lucilla', name: 'Lucilla', element: 'Glacio', weaponType: 'Rectifier', rarity: 5, image: '' },
+
   { id: 'sanhua', name: 'Sanhua', element: 'Glacio', weaponType: 'Sword', rarity: 4, image: '' },
-  { id: 'suisui', name: 'Suisui', element: 'Glacio', weaponType: 'Rectifier', rarity: 5, image: 'https://pin.it/5KsJ8gdUi' },
+
+  { id: 'suisui', name: 'Suisui', element: 'Glacio', weaponType: 'Rectifier', rarity: 5, image: '' }
+, // 3.5
+
   { id: 'youhu', name: 'Youhu', element: 'Glacio', weaponType: 'Gauntlets', rarity: 4, image: '' },
-  { id: 'zhezhi', name: 'Zhezhi', element: 'Glacio', weaponType: 'Rectifier', rarity: 5, image: 'https://pin.it/4jRG1tvcK' },
+
+  { id: 'zhezhi', name: 'Zhezhi', element: 'Glacio', weaponType: 'Rectifier', rarity: 5, image: '' },
+
+
   // Aero
   { id: 'aalto', name: 'Aalto', element: 'Aero', weaponType: 'Pistols', rarity: 4, image: '' },
-  { id: 'cartethyia', name: 'Cartethyia', element: 'Aero', weaponType: 'Sword', rarity: 5, image: 'https://pin.it/2P6OQ8uPE' },
+
+  { id: 'cartethyia', name: 'Cartethyia', element: 'Aero', weaponType: 'Sword', rarity: 5, image: '' },
+
+
   { id: 'ciaccona', name: 'Ciaccona', element: 'Aero', weaponType: 'Pistols', rarity: 5, image: '' },
-  { id: 'iuno', name: 'Iuno', element: 'Aero', weaponType: 'Gauntlets', rarity: 5, image: 'https://pin.it/6RrADkN33' },
+
+
+  { id: 'iuno', name: 'Iuno', element: 'Aero', weaponType: 'Gauntlets', rarity: 5, image: '' },
+
+
   { id: 'jianxin', name: 'Jianxin', element: 'Aero', weaponType: 'Gauntlets', rarity: 5, image: '' },
-  { id: 'jiyan', name: 'Jiyan', element: 'Aero', weaponType: 'Broadblade', rarity: 5, image: 'https://pin.it/4L04bUJ9Q' },
-  { id: 'qiuyuan', name: 'Qiuyuan', element: 'Aero', weaponType: 'Sword', rarity: 5, image: 'https://pin.it/4LUqAgyhs' },
-  { id: 'rover-aero', name: 'Rover (Aero)', element: 'Aero', weaponType: 'Sword', rarity: 5, image: '' },
-  { id: 'sigrika', name: 'Sigrika', element: 'Aero', weaponType: 'Gauntlets', rarity: 5, image: 'https://pin.it/44svE7shp' },
+
+
+  { id: 'jiyan', name: 'Jiyan', element: 'Aero', weaponType: 'Broadblade', rarity: 5, image: '' },
+
+
+  { id: 'qiuyuan', name: 'Qiuyuan', element: 'Aero', weaponType: 'Sword', rarity: 5, image: '' },
+
+
+  { id: 'sigrika', name: 'Sigrika', element: 'Aero', weaponType: 'Gauntlets', rarity: 5, image: '' },
+
+
   { id: 'yangyang', name: 'Yangyang', element: 'Aero', weaponType: 'Sword', rarity: 4, image: '' },
+
+
+  { id: 'rover_aero', name: 'Rover (Aero)', element: 'Aero', weaponType: 'Sword', rarity: 5, image: '' },
+
+
   // Electro
-  { id: 'augusta', name: 'Augusta', element: 'Electro', weaponType: 'Broadblade', rarity: 5, image: 'https://pin.it/7MpdMQODw' },
+  { id: 'augusta', name: 'Augusta', element: 'Electro', weaponType: 'Broadblade', rarity: 5, image: '' },
+
+
   { id: 'buling', name: 'Buling', element: 'Electro', weaponType: 'Rectifier', rarity: 4, image: '' },
+
+
   { id: 'calcharo', name: 'Calcharo', element: 'Electro', weaponType: 'Broadblade', rarity: 5, image: '' },
-  { id: 'lumi', name: 'Lumi', element: 'Electro', weaponType: 'Sword', rarity: 4, image: '' },
-  { id: 'rebecca', name: 'Rebecca', element: 'Electro', weaponType: 'Pistols', rarity: 4, image: '' },
-  { id: 'xiangli-yao', name: 'Xiangli Yao', element: 'Electro', weaponType: 'Gauntlets', rarity: 5, image: 'https://pin.it/3TxERox9R' },
-  { id: 'yinlin', name: 'Yinlin', element: 'Electro', weaponType: 'Rectifier', rarity: 5, image: 'https://pin.it/4DAICfLZl' },
+
+
+  { id: 'lumi', name: 'Lumi', element: 'Electro', weaponType: 'Broadblade', rarity: 4, image: '' },
+
+
+  { id: 'rebecca', name: 'Rebecca', element: 'Electro', weaponType: 'Pistols', rarity: 5, image: '' },
+
+
+  { id: 'xiangli_yao', name: 'Xiangli Yao', element: 'Electro', weaponType: 'Gauntlets', rarity: 5, image: '' },
+
+
+  { id: 'yinlin', name: 'Yinlin', element: 'Electro', weaponType: 'Sword', rarity: 5, image: '' },
+
+
   { id: 'yuanwu', name: 'Yuanwu', element: 'Electro', weaponType: 'Gauntlets', rarity: 4, image: '' },
-  // Spectro
-  { id: 'jinhsi', name: 'Jinhsi', element: 'Spectro', weaponType: 'Broadblade', rarity: 5, image: 'https://pin.it/6meHWfFQ7' },
-  { id: 'lucy', name: 'Lucy', element: 'Spectro', weaponType: 'Pistols', rarity: 5, image: 'https://pin.it/4Cy8K8yWe' },
-  { id: 'luuk-herssen', name: 'Luuk Herssen', element: 'Spectro', weaponType: 'Gauntlets', rarity: 5, image: 'https://pin.it/6VrnfdTxn' },
-  { id: 'lynae', name: 'Lynae', element: 'Spectro', weaponType: 'Pistols', rarity: 5, image: 'https://pin.it/3Z0aO0Do2' },
-  { id: 'phoebe', name: 'Phoebe', element: 'Spectro', weaponType: 'Rectifier', rarity: 5, image: 'https://pin.it/5sf2VB6Eg' },
-  { id: 'rover-spectro', name: 'Rover (Spectro)', element: 'Spectro', weaponType: 'Sword', rarity: 5, image: '' },
-  { id: 'shorekeeper', name: 'The Shorekeeper', element: 'Spectro', weaponType: 'Rectifier', rarity: 5, image: 'https://pin.it/1lk2DfGKP' },
-  { id: 'verina', name: 'Verina', element: 'Spectro', weaponType: 'Rectifier', rarity: 5, image: '' },
-  { id: 'zani', name: 'Zani', element: 'Spectro', weaponType: 'Gauntlets', rarity: 5, image: 'https://pin.it/5xR1b8rhs' },
-  // Havoc
-  { id: 'camellya', name: 'Camellya', element: 'Havoc', weaponType: 'Sword', rarity: 5, image: 'https://pin.it/7i15EPf75' },
-  { id: 'cantarella', name: 'Cantarella', element: 'Havoc', weaponType: 'Rectifier', rarity: 5, image: 'https://pin.it/5JuskVmCt' },
-  { id: 'chisa', name: 'Chisa', element: 'Havoc', weaponType: 'Broadblade', rarity: 5, image: '' },
-  { id: 'danjin', name: 'Danjin', element: 'Havoc', weaponType: 'Sword', rarity: 4, image: '' },
-  { id: 'phrolova', name: 'Phrolova', element: 'Havoc', weaponType: 'Rectifier', rarity: 5, image: 'https://pin.it/6YJDvbaIL' },
-  { id: 'roccia', name: 'Roccia', element: 'Havoc', weaponType: 'Gauntlets', rarity: 5, image: 'https://pin.it/17gGbGe6f' },
-  { id: 'rover-havoc', name: 'Rover (Havoc)', element: 'Havoc', weaponType: 'Sword', rarity: 5, image: '' },
-  // Fusion (Note: Remaining roster slots for Fusion units complete the set of 53)
+
+
+
+  // Fusion
+  { id: 'aemeath', name: 'Aemeath', element: 'Fusion', weaponType: 'Sword', rarity: 5, image: '' },
+
+
+  { id: 'brant', name: 'Brant', element: 'Fusion', weaponType: 'Sword', rarity: 5, image: '' },
+
+  { id: 'changli', name: 'Changli', element: 'Fusion', weaponType: 'Sword', rarity: 5, image: '' },
+
   { id: 'chixia', name: 'Chixia', element: 'Fusion', weaponType: 'Pistols', rarity: 4, image: '' },
+
+  { id: 'denia', name: 'Denia', element: 'Fusion', weaponType: 'Rectifier', rarity: 5, image: '' },
+
   { id: 'encore', name: 'Encore', element: 'Fusion', weaponType: 'Rectifier', rarity: 5, image: '' },
-  { id: 'mortefi', name: 'Mortefi', element: 'Fusion', weaponType: 'Pistols', rarity: 4, image: 'https://pin.it/1irnzWiS9' },
+
+  { id: 'galbrena', name: 'Galbrena', element: 'Fusion', weaponType: 'Pistols', rarity: 5, image: '' },
+
+  { id: 'lupa', name: 'Lupa', element: 'Fusion', weaponType: 'Broadblade', rarity: 5, image: '' },
+
+
+  { id: 'mornye', name: 'Mornye', element: 'Fusion', weaponType: 'Broadblade', rarity: 5, image: '' },
+
+  { id: 'mortefi', name: 'Mortefi', element: 'Fusion', weaponType: 'Pistols', rarity: 4, image: '' },
+
+
+  // Havoc
+  { id: 'camellya', name: 'Camellya', element: 'Havoc', weaponType: 'Sword', rarity: 5, image: '' },
+
+
+  { id: 'cantarella', name: 'Cantarella', element: 'Havoc', weaponType: 'Rectifier', rarity: 5, image: '' },
+
+
+  { id: 'chisa', name: 'Chisa', element: 'Havoc', weaponType: 'Broadblade', rarity: 5, image: '' },
+
+
+  { id: 'danjin', name: 'Danjin', element: 'Havoc', weaponType: 'Sword', rarity: 4, image: '' },
+
+
+  { id: 'phrolova', name: 'Phrolova', element: 'Havoc', weaponType: 'Rectifier', rarity: 5, image: '' },
+
+
+  { id: 'roccia', name: 'Roccia', element: 'Havoc', weaponType: 'Gauntlets', rarity: 5, image: '' },
+
+
   { id: 'taoqi', name: 'Taoqi', element: 'Havoc', weaponType: 'Broadblade', rarity: 4, image: '' },
-  { id: 'changli', name: 'Changli', element: 'Fusion', weaponType: 'Sword', rarity: 5, image: 'https://pin.it/55I8tkZDw' },
-{ id: 'mornye', name: 'Mornye', element: 'Fusion', weaponType: 'brkadblade', rarity: 5, 'https://pin.it/3zjE4WSZ9' },
-  // ... (Additional characters from Version 3.x cycle)
+
+
+  { id: 'rover_havoc', name: 'Rover (Havoc)', element: 'Havoc', weaponType: 'Sword', rarity: 5, image: '' },
+
+
+  { id: 'yangyang_xuanling', name: 'Yangyang: Xuanling', element: 'Havoc', weaponType: 'Sword', rarity: 5, image: '' }, // 3.5
+
+  // Spectro
+  { id: 'jinhsi', name: 'Jinhsi', element: 'Spectro', weaponType: 'Broadblade', rarity: 5, image: '' },
+
+  { id: 'lucy', name: 'Lucy', element: 'Spectro', weaponType: 'Pistols', rarity: 5, image: '' },
+
+
+  { id: 'luuk_herssen', name: 'Luuk Herssen', element: 'Spectro', weaponType: 'Gauntlets', rarity: 5, image: '' },
+
+
+  { id: 'lynae', name: 'Lynae', element: 'Spectro', weaponType: 'Pistols', rarity: 5, image: '' },
+
+
+  { id: 'phoebe', name: 'Phoebe', element: 'Spectro', weaponType: 'Rectifier', rarity: 5, image: '' },
+
+
+  { id: 'shorekeeper', name: 'The Shorekeeper', element: 'Spectro', weaponType: 'Rectifier', rarity: 5, image: '' },
+
+
+  { id: 'verina', name: 'Verina', element: 'Spectro', weaponType: 'Rectifier', rarity: 5, image: '' },
+
+
+  { id: 'zani', name: 'Zani', element: 'Spectro', weaponType: 'Broadblade', rarity: 5, image: '' },
+
+
+  { id: 'rover_spectro', name: 'Rover (Spectro)', element: 'Spectro', weaponType: 'Sword', rarity: 5, image: '' },
+
+
+  // Announced / not yet released (3.5+) — element & weapon not officially confirmed yet
+  { id: 'suoming', name: 'Suoming', element: '', weaponType: '', rarity: 5, image: '' },
+  { id: 'jingran', name: 'Jingran', element: '', weaponType: '', rarity: 5, image: '' },
+  { id: 'qingxiao', name: 'Qingxiao', element: '', weaponType: '', rarity: 5, image: '' },
+  { id: 'hsin', name: 'Hsin', element: '', weaponType: '', rarity: 5, image: '' },
 ];
