4437061f0b433c6658fdbe75062c5c847995c60a
Jin <simplord.189119@gmail.com>
2026-07-03 18:51:36 +0530

    Update weaponsData.js

--- src/weaponsData.js
+++ src/weaponsData.js
@@ -1,25 +1,25 @@
 export const WEAPONS = [
   // --- Standard 5-Star Weapons ---
-  { id: 'standard_sword', name: 'Emerald of Genesis', type: 'Sword', rarity: 5, image: '/weapons/emeraldofgenesis.jpg' },
+  { id: 'standard_sword', name: 'Emerald of Genesis', type: 'Sword', rarity: 5, image: '' },
 
-  { id: 'standard_rectifier', name: 'Cosmic Ripples', type: 'Rectifier', rarity: 5, image: '/weapons/cosmicripples.jpg' },
+  { id: 'standard_rectifier', name: 'Cosmic Ripples', type: 'Rectifier', rarity: 5, image: '' },
 
-  { id: 'standard_pistol', name: 'Static Mist', type: 'Pistols', rarity: 5, image: '/weapons/staticmist.jpg' },
+  { id: 'standard_pistol', name: 'Static Mist', type: 'Pistols', rarity: 5, image: '' },
 
-  { id: 'standard_broadblade', name: 'Lustrous Razor', type: 'Broadblade', rarity: 5, image: '/weapons/lustrousrazor.jpg' },
+  { id: 'standard_broadblade', name: 'Lustrous Razor', type: 'Broadblade', rarity: 5, image: '' },
 
-  { id: 'standard_gauntlet', name: 'Abyss Surges', type: 'Gauntlets', rarity: 5, image: '/weapons/abysssurges.jpg' },
+  { id: 'standard_gauntlet', name: 'Abyss Surges', type: 'Gauntlets', rarity: 5, image '' },
 
   // --- Limited / Signature 5-Star Weapons ---
   // Glacio
-  { id: 'sig_carlotta', name: 'The Last Dance', type: 'Pistols', rarity: 5, image: '/weapons/the lastdance.jpg' }, // Carlotta
+  { id: 'sig_carlotta', name: 'The Last Dance', type: 'Pistols', rarity: 5, image: '/weapons/thelastdance.jpg' }, // Carlotta
 
   { id: 'sig_hiyuki', name: 'Frostburn', type: 'Sword', rarity: 5, image: '/weapons/frostburn.jpg' }, // Hiyuki
 
   { id: 'sig_lucilla', name: 'Freeze Frame', type: 'Rectifier', rarity: 5, image: '/weapons/freezeframe.jpg' }, // Lucilla
 
 
-  { id: 'sig_suisui', name: "Firstlight's Herald", type: 'Rectifier', rarity: 5, image: '/weapons/firstlightsherald.jpg' }, // Suisui
+  { id: 'sig_suisui', name: "Firstlight's Herald", type: 'Rectifier', rarity: 5, image: '' }, // Suisui
 
 
   { id: 'sig_zhezhi', name: 'Rime-Draped Sprouts', type: 'Rectifier', rarity: 5, image: '/weapons/rimedrapedsprouts.jpg' }, // Zhezhi
@@ -33,44 +33,44 @@ export const WEAPONS = [
   { id: 'sig_ciaccona', name: 'Woodland Aria', type: 'Pistols', rarity: 5, image: '/weapons/woodlandaria.jpg' }, // Ciaccona
 
 
-  { id: 'sig_iuno', name: "Moongazer's Sigil", type: 'Gauntlets', rarity: 5, image: '/weapons/moongazerssigil.jpg' }, // Iuno
+  { id: 'sig_iuno', name: "Moongazer's Sigil", type: 'Gauntlets', rarity: 5, image: '' }, // Iuno
 
 
-  { id: 'sig_jiyan', name: 'Verdant Summit', type: 'Broadblade', rarity: 5, image: '/weapons/verdantsummit.jpg' }, // Jiyan
+  { id: 'sig_jiyan', name: 'Verdant Summit', type: 'Broadblade', rarity: 5, image: '' }, // Jiyan
 
 
-  { id: 'sig_qiuyuan', name: 'Emerald Sentence', type: 'Sword', rarity: 5, image: '/weapons/emeraldsentence.jpg' }, // Qiuyuan
+  { id: 'sig_qiuyuan', name: 'Emerald Sentence', type: 'Sword', rarity: 5, image: '' }, // Qiuyuan
 
 
-  { id: 'sig_sigrika', name: 'Solsworn Ciphers', type: 'Gauntlets', rarity: 5, image: '/weapons/solswormciphers.jpg' }, // Sigrika
+  { id: 'sig_sigrika', name: 'Solsworn Ciphers', type: 'Gauntlets', rarity: 5, image: '' }, // Sigrika
 
 
-  { id: 'sig_rover_aero', name: "Bloodpact's Pledge", type: 'sword', rarity: 5, image: '/weapons/bloodpactpledge.jpg' }, // Rover (Aero)
+  { id: 'sig_rover_aero', name: "Bloodpact's Pledge", type: 'sword', rarity: 5, image: '' }, // Rover (Aero)
 
 
 
   // Electro
-  { id: 'sig_augusta', name: 'Thunderflare Dominion', type: 'Broadblade', rarity: 5, image: '/weapons/thunderflaredominion.jpg' }, // Augusta
+  { id: 'sig_augusta', name: 'Thunderflare Dominion', type: 'Broadblade', rarity: 5, image: '' }, // Augusta
 
 
-  { id: 'sig_rebecca', name: 'Skull Thrasher', type: 'Pistols', rarity: 5, image: '/weapons/skullthrasher.jpg' }, // Rebecca
+  { id: 'sig_rebecca', name: 'Skull Thrasher', type: 'Pistols', rarity: 5, image: '' }, // Rebecca
 
 
   { id: 'sig_xiangli_yao', name: "Verity's Handle", type: 'Gauntlets', rarity: 5, image: '/weapons/verityshandle.jpg' }, // Xiangli Yao
 
 
-  { id: 'sig_yinlin', name: 'Stringmaster', type: 'Rectifier', rarity: 5, image: '/weapons/stringmaster.jpg' }, // Yinlin
+  { id: 'sig_yinlin', name: 'Stringmaster', type: 'Rectifier', rarity: 5, image: '' }, // Yinlin
 
 
 
   // Fusion
-  { id: 'sig_aemeath', name: 'Everbright Polestar', type: 'Sword', rarity: 5, image: '/weapons/everbrightpolestar.jpg' }, // Aemeath
+  { id: 'sig_aemeath', name: 'Everbright Polestar', type: 'Sword', rarity: 5, image: '' }, // Aemeath
 
 
-  { id: 'sig_changli', name: 'Blazing Brilliance', type: 'Sword', rarity: 5, image: '/weapons/blazingbrilliance' }, // Changli
+  { id: 'sig_changli', name: 'Blazing Brilliance', type: 'Sword', rarity: 5, image: '' }, // Changli
 
 
-  { id: 'sig_denia', name: 'Forged Dwarf Star', type: 'Rectifier', rarity: 5, image: '/weapons/forgeddwarfstar.jpg' }, // Denia
+  { id: 'sig_denia', name: 'Forged Dwarf Star', type: 'Rectifier', rarity: 5, image: '' }, // Denia
 
 
   { id: 'sig_galbrena', name: 'Lux & Umbra', type: 'Pistols', rarity: 5, image: '/weapons/lux&umbra.jpg' }, // Galbrena
@@ -85,7 +85,7 @@ export const WEAPONS = [
   { id: 'sig_cantarella', name: 'Whispers of Sirens', type: 'Rectifier', rarity: 5, image: '/weapons/whispersofsirens' }, // Cantarella
 
 
-  { id: 'sig_chisa', name: 'Kumokiri', type: 'Broadblade', rarity: 5, image: '/weapons/kumokiri.jpg' }, // Chisa
+  { id: 'sig_chisa', name: 'Kumokiri', type: 'Broadblade', rarity: 5, image: '' }, // Chisa
 
 
   { id: 'sig_phrolova', name: 'Lethean Elegy', type: 'Rectifier', rarity: 5, image: '/weapons/letheanelegy.jpg' }, // Phrolova
@@ -94,22 +94,22 @@ export const WEAPONS = [
   { id: 'sig_roccia', name: 'Tragicomedy', type: 'Gauntlets', rarity: 5, image: '/weapons/tragicomedy.jpg' }, // Roccia
 
 
-  { id: 'sig_yangyang_xuanling', name: 'Azure Oath', type: 'Sword', rarity: 5, image: '/weapons/azureoath.jpg' }, // Yangyang: Xuanling
+  { id: 'sig_yangyang_xuanling', name: 'Azure Oath', type: 'Sword', rarity: 5, image: '' }, // Yangyang: Xuanling
 
 
 
   // Spectro
-  { id: 'sig_jinhsi', name: 'Ages of Harvest', type: 'Broadblade', rarity: 5, image: '/weapons/agesofharvest.jpg' }, // Jinhsi
+  { id: 'sig_jinhsi', name: 'Ages of Harvest', type: 'Broadblade', rarity: 5, image: '' }, // Jinhsi
 
-  { id: 'sig_lucy', name: 'Spectral Trigger', type: 'Pistols', rarity: 5, image: '/weapons/spectraltrigger.jpg' }, // Lucy
+  { id: 'sig_lucy', name: 'Spectral Trigger', type: 'Pistols', rarity: 5, image: '' }, // Lucy
 
   { id: 'sig_luuk_herssen', name: "Daybreaker's Spine", type: 'Gauntlets', rarity: 5, image: '/weapons/daybreakersspine.jpg' }, // Luuk Herssen
 
   { id: 'sig_lynae', name: 'Spectrum Blaster', type: 'Pistols', rarity: 5, image: '/weapons/spectrumblaster.jpg' }, // Lynae
 
-  { id: 'sig_phoebe', name: 'Luminous Hymn', type: 'Rectifier', rarity: 5, image: '/weapons/luminoushymn.jpg' }, // Phoebe
+  { id: 'sig_phoebe', name: 'Luminous Hymn', type: 'Rectifier', rarity: 5, image: '' }, // Phoebe
 
-  { id: 'sig_shorekeeper', name: 'Stellar Symphony', type: 'Rectifier', rarity: 5, image: '/weapons/stellarsymphony.jpg' }, // The Shorekeeper
+  { id: 'sig_shorekeeper', name: 'Stellar Symphony', type: 'Rectifier', rarity: 5, image: '' }, // The Shorekeeper
 
-  { id: 'sig_zani', name: 'Blazing Justice', type: 'Gauntlets', rarity: 5, image: '/weapons/blazingjustice.jpg' }, // Zani
+  { id: 'sig_zani', name: 'Blazing Justice', type: 'Gauntlets', rarity: 5, image: '' }, // Zani
 ];
