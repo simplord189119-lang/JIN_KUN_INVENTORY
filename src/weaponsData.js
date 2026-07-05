4940d731bdd65a698316d962cb1d8594466b303a
Jin <simplord.189119@gmail.com>
2026-07-05 16:46:46 +0530

    Update weaponsData.js

--- src/weaponsData.js
+++ src/weaponsData.js
@@ -1,17 +1,34 @@
 export const WEAPONS = [
   // --- Standard 5-Star Weapons ---
   { id: 'standard_sword', name: 'Emerald of Genesis', type: 'Sword', rarity: 5, image: '/images/weapons/emerald_of_genesis.jpg' },
+
+
   { id: 'standard_rectifier', name: 'Cosmic Ripples', type: 'Rectifier', rarity: 5, image: '' }, // TODO: upload image
+
+
   { id: 'standard_pistol', name: 'Static Mist', type: 'Pistols', rarity: 5, image: '/images/weapons/static_mist.jpg' },
+
+
   { id: 'standard_broadblade', name: 'Lustrous Razor', type: 'Broadblade', rarity: 5, image: '/images/weapons/lustrous_razor.jpg' },
+
   { id: 'standard_gauntlet', name: 'Abyss Surges', type: 'Gauntlets', rarity: 5, image: '' }, // TODO: upload image
 
   // --- Standard 5-Star Weapons (Synth Armament Series, added in Version 3.0) ---
-  { id: 'standard_laser_shearer', name: 'Laser Shearer', type: 'Sword', rarity: 5, image: '' }, // TODO: upload image
+  { id: 'standard_laser_shearer', name: 'Laser Shearer', type: 'Sword', rarity: 5, image: '' }, 
+
+
   { id: 'standard_radiance_cleaver', name: 'Radiance Cleaver', type: 'Broadblade', rarity: 5, image: '' }, // TODO: upload image
+
+
   { id: 'standard_phasic_homogenizer', name: 'Phasic Homogenizer', type: 'Pistols', rarity: 5, image: '' }, // TODO: upload image
+
+
   { id: 'standard_boson_astrolabe', name: 'Boson Astrolabe', type: 'Rectifier', rarity: 5, image: '' }, // TODO: upload image
+
+
   { id: 'standard_pulsation_bracer', name: 'Pulsation Bracer', type: 'Gauntlets', rarity: 5, image: '' }, // TODO: upload image
+
+
 
   // --- Limited / Signature 5-Star Weapons ---
   // Glacio
